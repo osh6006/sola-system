c1993830096eed296fc2e2021865a5ad93065b1f
Hwang Seok O <ohs6006@gmail.com>
2023-06-28 21:58:28 +0900

    earth

--- src/js/planet.js
+++ src/js/planet.js
@@ -10,7 +10,9 @@ export class Planet {
   constructor(name) {
     this.name = name;
     this.leftEl = document.querySelector(`.${name} .planet__desc-left`);
-    this.leftBottomEl = document.querySelector(`.${name} .planet__desc-bottom-left`);
+    this.leftBottomEl = document.querySelector(
+      `.${name} .planet__desc-bottom-left`
+    );
     this.rightEl = document.querySelector(`.${name} .planet__desc-right`);
     this.flag = true;
   }
@@ -27,13 +29,13 @@ export class Planet {
     const rightTitleEl = this.rightEl.querySelector(".three");
     const rightDescEl = this.rightEl.querySelector(".container");
 
-    gsap.from(leftTitleEl, { y: -50, opacity: 0, delay: 1 });
-    gsap.from(leftBottomTitleEl, { y: -50, opacity: 0, delay: 2 });
-    gsap.from(rightTitleEl, { y: -50, opacity: 0, delay: 2 });
+    gsap.from(leftTitleEl, { y: -50, opacity: 0, delay: 0.3 });
+    gsap.from(leftBottomTitleEl, { y: -50, opacity: 0, delay: 0.5 });
+    gsap.from(rightTitleEl, { y: -50, opacity: 0, delay: 0.5 });
 
-    gsap.from(leftDescEl, { x: -50, opacity: 0, delay: 2 });
-    gsap.from(leftBottomDescEl, { x: -50, opacity: 0, delay: 3 });
-    gsap.from(rightDescEl, { x: -50, opacity: 0, delay: 3 });
+    gsap.from(leftDescEl, { x: -50, opacity: 0, delay: 0.5 });
+    gsap.from(leftBottomDescEl, { x: -50, opacity: 0, delay: 0.8 });
+    gsap.from(rightDescEl, { x: -50, opacity: 0, delay: 0.8 });
   }
 
   hideAnime() {
--- src/js/scroll.js
+++ src/js/scroll.js
@@ -7,6 +7,12 @@ import { Planet } from "./planet";
   const mercury = new Planet("mercury");
   const venus = new Planet("venus");
   const earth = new Planet("earth");
+  const mars = new Planet("mars");
+  const jupiter = new Planet("jupiter");
+  const saturn = new Planet("saturn");
+  const uranus = new Planet("uranus");
+  const neptune = new Planet("neptune");
+  const pluto = new Planet("pluto");
 
   const stageEl = document.querySelector(".world .stage");
   const progressEl = document.querySelector(".progress-bar");
@@ -19,7 +25,7 @@ import { Planet } from "./planet";
 
   window.addEventListener("scroll", () => {
     //  스크롤할 때 얼만큼 스크롤 했는지의 비율
-    const zMove = (window.scrollY / maxScrollValue) * 980 - 490;
+    const zMove = (window.scrollY / maxScrollValue) * 2000 - 490;
     stageEl.style.transform = `translateZ(${zMove}vw)`;
 
     // // 프로그래스바
@@ -30,24 +36,49 @@ import { Planet } from "./planet";
 
     const scrollPoint = (window.scrollY / maxScrollValue) * 100;
     console.log(scrollPoint);
-    if (scrollPoint > 10 && sun.flag) {
+    if (scrollPoint > 3 && sun.flag) {
       sun.makeAnime();
       sun.setFlag();
     }
 
-    if (scrollPoint > 35 && mercury.flag) {
+    if (scrollPoint > 12 && mercury.flag) {
       mercury.makeAnime();
       mercury.setFlag();
     }
 
-    if (scrollPoint > 70 && venus.flag) {
+    if (scrollPoint > 22 && venus.flag) {
       venus.makeAnime();
       venus.setFlag();
     }
 
-    if (scrollPoint > 90 && earth.flag) {
+    if (scrollPoint > 32 && earth.flag) {
       earth.makeAnime();
       earth.setFlag();
+    }
+
+    if (scrollPoint > 42 && mars.flag) {
+      mars.makeAnime();
+      mars.setFlag();
+    }
+    if (scrollPoint > 52 && jupiter.flag) {
+      jupiter.makeAnime();
+      jupiter.setFlag();
+    }
+    if (scrollPoint > 62 && saturn.flag) {
+      saturn.makeAnime();
+      saturn.setFlag();
+    }
+    if (scrollPoint > 72 && uranus.flag) {
+      uranus.makeAnime();
+      uranus.setFlag();
+    }
+    if (scrollPoint > 82 && neptune.flag) {
+      neptune.makeAnime();
+      neptune.setFlag();
+    }
+    if (scrollPoint > 92 && pluto.flag) {
+      pluto.makeAnime();
+      pluto.setFlag();
     }
   });
 
